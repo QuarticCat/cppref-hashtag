be3e4c7cd45acedf1fafe0c7e4cad54af5112824
QuarticCat <QuarticCat@protonmail.com>
2021-06-12 11:38:05 +0800

    add metadata and padding

--- src/meta.js
+++ src/meta.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         process.env.NAME
-// @namespace    mailto:QuarticCat@protonmail.com
 // @version      process.env.VERSION
 // @description  process.env.DESCRIPTION
 // @author       process.env.AUTHOR
@@ -8,4 +7,7 @@
 // @icon         https://www.google.com/s2/favicons?domain=cppreference.com
 // @require      https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1
 // @license      MIT
+// @namespace    https://github.com/QuarticCat
+// @homepageURL  https://github.com/QuarticCat/cppref-hashtag
+// @supportURL   https://greasyfork.org/scripts/427790
 // ==/UserScript==
